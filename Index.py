7ac5caaf0431748ab98d39114797e14e1488ec55
arsenibyk <37446248+arsenibyk@users.noreply.github.com>
2019-04-05 12:56:46 +0300

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,23 @@
+import karta_api
+
+def card(card_handle):
+    bc = user_balance()
+    
+    users{
+    user='arsenibyk'
+    host='185938798130'
+    pass='9336189c'
+    bc = '12000000000'
+    }
+    
+    if new_transfer:
+        bc+transfer_money
+    
+    elif transfer_to:
+        bc-to_transfer_money
+    
+    elif buy:
+        bc-buy_money
+        
+while 1:
+    print(bc)
